c55838cac653f295b78798b0e1daf12e899f8489
danieljbell <djbell70@gmail.com>
2018-01-30 16:33:49 -0500

    process port var

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello Fam!');
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
