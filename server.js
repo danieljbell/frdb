f1ac7d07e3d8442487d2fe1e57561d07f762f996
danieljbell <djbell70@gmail.com>
2018-01-30 17:00:07 -0500

    reconfigure again

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello Fam!');
+  res.render('layout');
 });
 
-const port = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
