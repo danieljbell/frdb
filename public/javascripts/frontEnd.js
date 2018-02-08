f06c6b00b256e669f426522946dc72989a09fd1b
danieljbell <djbell70@gmail.com>
2018-02-07 23:15:56 -0500

    add and remove ingredients

--- public/javascripts/frontEnd.js
+++ public/javascripts/frontEnd.js
@@ -1,5 +1,24 @@
 import '../sass/style.scss';
+import axios from 'axios';
 
-const thing = `Hey Man!`;
+const ingredient = document.querySelector('.add-ingredient');
+const removeIngredient = document.querySelectorAll('.remove-ingredient');
 
-console.log(thing);
+if (ingredient) {
+  ingredient.addEventListener('click', function(e) {
+    e.preventDefault();
+    const html = `
+      <label>Ingredient</label>
+      <input type="text" name="ingredients">
+      <button class="add-ingredient">+</button>
+    `;
+    ingredient.parentElement.innerHTML += html;
+  });
+}
+
+removeIngredient.forEach((element) => {
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentElement.remove()
+  });
+});
